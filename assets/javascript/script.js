c4de9f67c60a77fa8d1b25061f46371b71bdba77
shualton <shualton@gmail.com>
2019-06-10 21:40:15 -0700

    initialized firebase database

---
+++ assets/javascript/script.js
@@ -0,0 +1,33 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCZ70epQwR1bmkOCrovGX9xQ55CHoN-Qrc",
+    authDomain: "super-original-project.firebaseapp.com",
+    databaseURL: "https://super-original-project.firebaseio.com",
+    projectId: "super-original-project",
+    storageBucket: "super-original-project.appspot.com",
+    messagingSenderId: "442170095409",
+    appId: "1:442170095409:web:4371a828a6f599ab"
+  };
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.database();
+  
+  
+  $('#addTrainBtn').on("click", function() {
+    var trainName = $("#trainNameInput").val().trim();
+    var destination = $("#destinationInput").val().trim();
+    var firstTrain = moment($("#timeInput").val().trim(), "HH:mm").format("HH:mm");
+    var frequency = $("#frequencyInput").val().trim();
+    var newTrain = {
+        name: trainName,
+        place: destination,
+        ftrain: firstTrain,
+        freq: frequency
+      }
+    database.ref().push(newTrain);
+    console.log(newTrain.name);
+    $("#trainNameInput").val("");
+    $("#destinationInput").val("");
+    $("#timeInput").val("");
+    $("#frequencyInput").val("");
+    return false;
+  });
+ 
