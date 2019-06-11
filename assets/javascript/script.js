c1f78a913d6e35c8b0b9b396e9a5b51ce1e84580
shualton <shualton@gmail.com>
2019-06-10 21:41:01 -0700

    added addTrain function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -30,4 +30,22 @@ var firebaseConfig = {
     $("#frequencyInput").val("");
     return false;
   });
- 
+  database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+    var trainName = childSnapshot.val().name;
+    var destination = childSnapshot.val().place;
+    var firstTrain = childSnapshot.val().ftrain;
+    var frequency = childSnapshot.val().freq;
+    var firstTimeConverted = moment(firstTrain, "HH:mm");
+    console.log(firstTimeConverted);
+    var currentTime = moment().format("HH:mm");
+    console.log("CURRENT TIME: " + currentTime);
+    var timeDiff = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log(firstTrain);
+    console.log("Difference in Time: " + timeDiff);
+    var timeRemainder = timeDiff % frequency;
+    console.log(timeRemainder);
+    var minToTrain = frequency - timeRemainder;
+    var nxTrain = moment().add(minToTrain, "minutes").format("HH:mm");
+    $("#trainTable>tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + nxTrain + "</td><td>" + frequency + "</td><td>" + minToTrain + "</td></tr>");
+  });
